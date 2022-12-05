e23f8b3b0ec5f8ca6fe8eb836ad8038e41940ac9
Mohitupa <mohitu531@gmail.com>
2022-12-05 11:33:57 +0530

    dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './modules/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "Dashboard", component: AdminComponent },
+  { path: "login", component: AdminComponent },
+  { path: "register", component: AdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/admin/components/dashboard/dashboard.component';
+import { RolesComponent } from './modules/admin/components/roles/roles.component';
+import { PermissionsComponent } from './modules/admin/components/permissions/permissions.component';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { RegistrationComponent } from './modules/auth/registration/registration.component';
+import { SidePanelComponent } from './modules/admin/pages/side-panel/side-panel.component';
+import { AdminHeaderComponent } from './modules/admin/pages/admin-header/admin-header.component';
+import { AdminFooterComponent } from './modules/admin/pages/admin-footer/admin-footer.component';
+import { AdminComponent } from './modules/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    RolesComponent,
+    PermissionsComponent,
+    LoginComponent,
+    RegistrationComponent,
+    SidePanelComponent,
+    AdminHeaderComponent,
+    AdminFooterComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
